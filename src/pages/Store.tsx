371ed3a0bc90d68cc0f2cc49306daa9c2c0e8377
JPLACLAU <laclaujeanpaul@gmail.com>
2023-03-17 23:03:52 -0300

    before removing currency format

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -8,6 +8,9 @@ export function Store() {
     <>
       <MetaData title="Store" />
       <h1>Store</h1>
+      <h3>All prices are in Satoshis</h3>
+      <h5>All items come in packs of 5x units.</h5>
+
       <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map(item => (
           <Col key={item.id}>
